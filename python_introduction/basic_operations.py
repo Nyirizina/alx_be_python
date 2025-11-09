29325c244d0efefb589ac14b3e3a1fe357d5f6ff
Manzi Ivan <manzi9ivan@gmail.com>
2025-11-09 04:44:25 +0200

    code update

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 sum = number1 + number2
 difference = number2 - number1
 multiply = number2 * number1
-print(Addition of , number1,and, number2,is, sum)
-print(Subtraction of , number2,and, number1,is, difference)
-print(Multiplication of , number1,and, number2,is, multiply)
+print("Addition of ", number1,"and", number2,"is", sum)
+print("Subtraction of" , number2,"and", number1,"is", difference)
+print("Multiplication of" , number1,"and", number2,"is", multiply)
