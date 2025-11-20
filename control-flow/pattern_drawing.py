c5b50be19cf61dca8721d9cb1472edf1736b43cd
Manzi Ivan <manzi9ivan@gmail.com>
2025-11-20 10:56:53 +0200

    patterns for square

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+row = int(input("Enter the size of the pattern:"))
+current_row = 0
+while current_row < row:
+    
+    for col in range(row):
+            print("*", end=" ")
+    print()
+    current_row += 1
+    
