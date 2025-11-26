b3c7b18b5468c1a561c70cec3e23ff302cac79b6
Manzi Ivan <manzi9ivan@gmail.com>
2025-11-26 13:53:32 +0200

    conversion of temperatures

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,9 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
-    FAHRENHEIT_TO_CELCIUS_FACTOR = 5.0 / 9.0
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELCIUS_FACTOR
     return celsius
 def convert_to_fahrenheit(celsius):
-    CELSIUS_TO_FAHRENHEIT_FACTOR = 9.0 / 5.0
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 def main():
