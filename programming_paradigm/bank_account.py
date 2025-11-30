40eaa42e0e4782883559b330f010d07a0569f767
Manzi Ivan <manzi9ivan@gmail.com>
2025-11-30 23:14:44 +0200

    new paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+        else:
+            raise ValueError("Deposit amount must be positive")
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current balance: ${self.balance:.2f RWF}")
