7687b1e350ac7681f655772d585e15d1a48f115e
Manzi Ivan <manzi9ivan@gmail.com>
2025-12-10 16:04:50 +0200

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"Generic Book: {self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
     def __init__(self,title,author,file_size: int):
         super().__init__(title,author)
@@ -15,7 +15,7 @@ class PrintBook(Book):
         super().__init__(title , author)
         self.page_count = page_count
     def __str__(self):
-        return f"Print:  '{self.title}' by {self.author} ({self.page_count} pages)"
+        return f"PrintBook:  '{self.title}' by {self.author} ({self.page_count} pages)"
 class Library:
     def __init__(self):
         self.books = []
