163c86db0ad600942cfe4d384c6c3118fc63eb3e
Manzi Ivan <manzi9ivan@gmail.com>
2025-12-10 15:47:25 +0200

    library system

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_details(self):
+        return f"Generic Book: {self.title} by {self.author}"
+class EBook(Book):
+    def __init__(self,title,author,file_size: int):
+        super().__init__(title,author)
+        self.file_size = file_size
+    def get_details(self):
+            return f"E-Book: '{self.title}' by {self.author} (Size: {self.file_size}MB)"
+class PrintBook(Book):
+    def __init__(self, title, author,page_count: int):
+        super().__init__(title , author)
+        self.page_count = page_count
+    def get_details(self):
+        return f"Print:  '{self.title}' by {self.author} ({self.page_count} pages)"
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Error")
+    def List_books(self):
+        for book in self.books:
+            print(book.get_details())
